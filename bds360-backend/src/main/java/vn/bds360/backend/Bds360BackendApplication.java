d3dcd3dd12e494b48b5f65618bd43e2b7fba1c92
zdat5624 <zdat5624@gmail.com>
2026-03-24 19:04:15 +0700

    init backend

---
+++ bds360-backend/src/main/java/vn/bds360/backend/Bds360BackendApplication.java
@@ -0,0 +1,13 @@
+package vn.bds360.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bds360BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bds360BackendApplication.class, args);
+	}
+
+}
